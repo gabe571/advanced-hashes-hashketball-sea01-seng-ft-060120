ddb302fcde25c27ace920b9740c549e02d8db8e9
Gabriel Castro <gabriel.castro1028@gmail.com>
2020-06-01 01:40:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def game_hash
   {
     home: {
@@ -125,71 +126,71 @@ def game_hash
   }
 end
 
-def num_points_scored(player_name)
-game_hash.values.each do |team_name|
-  team_name[:players].each do |player|
-      if player[:player_name] == player_name
-       return player[:points]
-      end
+def num_points_scored(players_name)
+  game_hash.each do |location, keys|
+    keys[:players].each do |player|
+      return player[:points] if player[:player_name] == player_name
     end
   end
 end
 
-def shoe_size(player_name)
-  game_hash.values.each do |team_name|
-    team_name[:players].each do |player|
-     if player[:player_name] == player_name
-      return player[:shoe]
-      end
+
+
+def shoe_size(player_n)
+  game_hash.each do |home_away, keys|
+    keys[:players].each do |player|
+      return player[:shoe] if player[:player_name] == player_n
     end
   end
 end
 
 def team_colors(team_name)
-  game_hash.values.each do |team|
-      if team[:team_name] == team_name
-        return team[:colors]
+  game_hash.each do |home_away, keys|
+    if keys[:team_name] == team_name
+      return keys[:colors].map(&:capitalize)
     end
   end
 end
 
 def team_names
-  game_hash.values.map {|name|
-  name[:team_name]
-  }
-end 
+  game_hash.map {|home_away, keys| keys[:team_name]}
+end
 
 def player_numbers(team_name)
- 
- game_hash.values.each do |team|
-   if team[:team_name] == team_name
-     return team[:players].map {|player|
-     player[:number]}
- end
-end
+  game_hash.each do |home_away, keys|
+    if keys[:team_name] == team_name
+      return keys[:players].map { |player| player[:number] }
+    end
+  end
 end
 
 def player_stats(player_name)
-  game_hash.values.each do |team_name|
-    team_name[:players].each do |player|
-      if player[:player_name] == player_name 
-        return player
-      end 
+  stats = {}
+  game_hash.each do |h_or_a, parent_hash|
+    parent_hash.each do |team_info, data|
+      if team_info == :players
+        data.each do |categories|
+          if player_name == categories[:player_name]
+            stats = categories
+          end
+        end
+      end
     end
-  end 
+  end
+  stats
 end
 
 def big_shoe_rebounds
-     big_shoe = 0 
-     rebounds = 0 
-     game_hash.values.each do |team|
-       team[:players].each do |player|
-         if player[:shoe] > big_shoe
-           big_shoe = player[:shoe]
-           rebounds = player[:rebounds]
-         end
-        end
+  biggest = 0
+  rebounds = 0
+  game_hash.each do |home_away, keys|
+    keys[:players].each do |player|
+      size = player[:shoe]
+      if size > biggest
+        biggest = size
+        rebounds = player[:rebounds]
       end
-    rebounds
-   end
-         
+    end
+  end
+  rebounds
+end
